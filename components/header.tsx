14f976d2855fa511b297f7b99c090d85ccbf4751
development <helotesting09@gmail.com>
2023-11-07 20:39:42 +0000

    working started

---
+++ components/header.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { motion } from 'framer-motion'
+import React from 'react'
+import Link from 'next/link';
+import { links } from '@/lib/data';
+
+export default function Header() {
+  return (
+    <header className='z-[999] relative'>
+        <motion.div className='fixed top-0 left-1/2 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] rounded-none backdrop-blur-[0.5rem] border border-opacity-40 h-[4.5rem] w-full sm:top-6 sm:h-[3.5rem] sm:rounded-full sm:w-[36rem]'
+        initial={{y: -100, x: "-50%", opacity: 0}}
+        animate={{y: 0, x: "-50%", opacity: 1}}
+        >
+        </motion.div>
+            <nav className='flex fixed sm:top-[2.2rem] h-[initial] left-1/2 -translate-x-1/2'>
+                <ul className='list-none m-0 p-0 flex gap-x-1 items-center justify-center text-[0.9rem] font-medium sm:w-[initial] flex-wrap sm:flex-nowrap w-[22rem]'>
+                    {links.map((link) => (
+                        <li key={link.name}>
+                            <Link href={link.hash} className='flex items-center decoration-none px-4 py-2 hover:text-gray-950 w-full'>{link.name}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+    </header>
+  )
+}
